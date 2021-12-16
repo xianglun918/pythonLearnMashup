58998765877b3164cd8fd09c4527782ac6c5c91c
xianglun918 <xianglun918@gmail.com>
2021-12-16 22:57:43 +0800

    TmpFix

--- DNA_Generator.py
+++ DNA_Generator.py
@@ -3,45 +3,55 @@
 '''
 @File    :   DNA_Generator.py
 @Time    :   2021/04/12 16:16:08
-@Author  :   xianglun xu 
+@Author  :   xianglun xu
 @Contact :   xianglun918@gmail.com
 '''
 
 # Libraries Used
-from ctypes import WINFUNCTYPE
-import pyperclip
+# from ctypes import WINFUNCTYPE
 import time
 
-# Decorator to show the running time.
-def timeCounter(fn):
+import pyperclip
+
+
+
+def time_counter(func):
+    '''
+    timeCounter:
+    Aims to decorate the function and count the costed time
+    '''
     def wrapper(*args, **kw):
         start_time = time.time()
-        result = fn(*args, **kw)
+        result = func(*args, **kw)
         end_time = time.time()
         print("Running time: %s seconds." % round(end_time - start_time, 2))
         return result
     return wrapper
 
 
-class DNA_Generator:
-    def __init__(self, text='None', direction='horizontal'):
+class DNAGenerator:
+    '''
+    Create spinning dna.
+    '''
+    def __init__(self, direction='horizontal'):
         while True:
-
             self._text = input("Input the content text or enter 'exit' to exit: ")
             if self._text == '':
                 print('Empty input is not acceptable !!!')
                 continue
             if self._text == 'help':
                 while True:
-                    mode = input("Current direction: %s. " % direction + 'You want to change to horizontal? [Yes|no]')
-                    if mode == '' or mode == 'Yes':
+                    mode = input("Current direction: %s. " % direction +
+                    'You want to change to horizontal? [Yes|no]')
+                    if mode in ('', 'Yes'):
                         direction = 'horizontal'
                         print('Got it. Direction changed to horizontal.')
                         break
-                    elif mode == 'no':
+                    if mode == 'no':
+                        print('Settings remain unchanged.')
                         break
-                    else:
-                        print('Invalid answer. Try again.')
+                    print("Try again. Input not acceptable.")
+
                 continue
 
 
@@ -54,17 +64,26 @@ class DNA_Generator:
 
     @property
     def text(self):
+        '''
+        Return the DNA text.
+        '''
         return self._text
 
     @text.setter
     def text(self, value):
+        '''
+        Set the DNA text.
+        '''
         if not isinstance(value, str):
             raise ValueError('The attribute input should be string type!')
         self._text = value
 
     # (Default) Draw vertically.
-    @timeCounter
+    @time_counter
     def draw(self, width=9, height=30):
+        '''
+        Print the DNA shape to the screen.
+        '''
         str_len = len(self._text)
         sub_matrix = []
         for i in range(height):
@@ -78,18 +97,23 @@ class DNA_Generator:
         pyperclip.copy(''.join(sub_matrix))
 
     # (Optional) Draw horizontally.
-    @timeCounter
+    @time_counter
     def draw_h(self, width=4, height=30):
+        '''
+        Draw horizontally if specified.
+        '''
         str_len = len(self._text)
         sub_matrix = []
         for i in range(height):
             tmp_index = i % (width*2)
             if tmp_index // width == 0:
-                left_str = ' ' * (tmp_index % width) + self._text + ' ' * (str_len + width - tmp_index % width)
+                left_str = ' ' * (tmp_index % width) + self._text +  \
+                ' ' * (str_len + width - tmp_index % width)
                 right_str = left_str[::-1]
                 sum_str = left_str + right_str
             else:
-                left_str = ' ' * (str_len + width - tmp_index % width) + self._text + ' ' * (tmp_index % width)
+                left_str = ' ' * (str_len + width - tmp_index % width) + \
+                self._text + ' ' * (tmp_index % width)
                 right_str = left_str[::-1]
                 sum_str = left_str + right_str
             sum_str += '\n'
@@ -100,4 +124,4 @@ class DNA_Generator:
 
 
 if __name__ == '__main__':
-    tmp = DNA_Generator()
+    tmp = DNAGenerator()
--- MyPackage/Animal.py
+++ MyPackage/Animal.py
@@ -3,11 +3,30 @@
 
 __author__ = "xianglun918"
 
-
-
+'''
+Class Dog()
+for creating an animal instance
+'''
 class Dog():
+    '''
+    This class is aimed to provide full functionality of a dog.
+    '''
     def __init__(self):
         print("This is a animal instance.")
+    @classmethod
+    def say_hello(cls):
+        ''' Just say Hello.
+        '''
+        print("Hello.")
+    @classmethod
+    def are_you_ok(cls):
+        '''
+        Say yo.
+        '''
+        print('Yo')
 
-def runTwice():
+
+def run_twice():
+    ''' Actually run twice.
+    '''
     print("Run twice as a fast animal.")
